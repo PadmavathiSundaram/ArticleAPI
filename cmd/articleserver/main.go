e58fbbfe5ff0fc4086fcae30f5eef47c01bf1f3f
padmavathi <padmavathi.sundaram@gmail.com>
2020-03-07 13:19:02 +1100

    Server Setup go setup with go mod init adding required dependecies - kingpin and go chi route setting up main.go with http server and chi router

---
+++ cmd/articleserver/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	mw "github.com/go-chi/chi/middleware"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	port = kingpin.Flag("port", "port").Short('p').Default("4852").Int()
+)
+
+func main() {
+	kingpin.Parse()
+	router := chi.NewRouter()
+	router.Use(mw.Logger)
+	server := &http.Server{
+		Handler: router,
+		Addr:    fmt.Sprintf(":%d", *port),
+	}
+	log.Infoln("Server listening on port", *port)
+	log.Fatal(server.ListenAndServe())
+}
